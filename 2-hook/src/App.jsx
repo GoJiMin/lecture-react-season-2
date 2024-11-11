f366dfc6b0c78e555ccb3ade74e00e5e5ecc73fa
GoJiMin <gojimin3095@gmail.com>
2024-11-11 18:00:15 +0900

    함수 컴포넌트로 다시 만들기 - ProductPage
    기존의 클래스형 컴포넌트를 함수 컴포넌트로 다시 구현합니다.
    - 클래스형 컴포넌트는 기본적으로 상태를 저장할 수 있습니다. (this.state)
    - 하지만 함수형 컴포넌트는 상태를 저장할 수 없습니다. 전달된 props를 통해 화면에 렌더링하는 순수함수로 동작하는게 특징입니다.
    - 그러나! 리액트에서 제공하는 useState 훅을 사용하면 상태를 저장할 수 있게 됩니다.
    
    어떻게 바뀔까요?
    - 기존의 생성자 함수에서 선언하던 this.state 대신 useState를 사용해 상태와 상태를 set할 수 있는 함수를 받습니다.
    - 그럼 이제 기존에 render 메서드를 사용해 화면에 그릴 때 this.state를 조회하지 않고 바로 productList를 사용합니다.
    
    부수효과는?
    - 메뉴 목록은 동적으로 화면에 표시됩니다. (productList)
    - 이 데이터는 서버에서 조회하는 데이터로 기존의 클래스형 컴포넌트에선 생명주기에 따라 컴포넌트가 마운트될 때 fetch 함수를 통해 데이터를 받아왔습니다.
    - fetch 함수는 현재 외부 데이터를 받아오는 부수효과입니다. 그럼 이전에 만들어봤던 useEffect을 사용합니다.
    - 컴포넌트가 마운트될 때 이 부수효과를 실행시키기 위해 의존성 배열을 빈 배열로 전달합니다.

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,69 +1,9 @@
-// const App = () => <>2-hook</>;
+import ProductPage from "./pages/ProductPage";
 
-// export default App;
+const App = () => (
+  <>
+    <ProductPage />
+  </>
+);
 
-import React from "react";
-import MyReact from "./lib/MyReact";
-
-const Counter = () => {
-  MyReact.resetCursor();
-
-  const [count, setCount] = React.useState(0);
-  const [name, setName] = React.useState("");
-
-  const handleClick = () => setCount(count + 1);
-
-  const handleChangeName = (e) => setName(e.target.value);
-
-  MyReact.useEffect(() => {
-    const storedName = localStorage.getItem("name") || "";
-    if (storedName !== name) {
-      setName(storedName);
-    }
-    console.log("effect3");
-  }, []);
-
-  MyReact.useEffect(() => {
-    document.title = `count: ${count} | name: ${name}`;
-    console.log("effect1");
-
-    return function cleanUp() {
-      document.title = "";
-      console.log("effect1 cleanUp");
-    };
-  }, [name, count]);
-
-  MyReact.useEffect(() => {
-    localStorage.setItem("name", name);
-    console.log("effect2");
-  }, [name]);
-
-  console.log("Counter rendered");
-
-  return (
-    <>
-      <input type="text" value={name} onChange={handleChangeName} />
-      <button onClick={handleClick}>더하기</button>
-    </>
-  );
-};
-
-export default () => {
-  const [mounted, setMounted] = React.useState(false);
-
-  const handleToggle = () => {
-    setMounted((prevMounted) => {
-      const nextMounted = !prevMounted;
-
-      if (!nextMounted) MyReact.cleanUpEffects();
-
-      return nextMounted;
-    });
-  };
-  return (
-    <>
-      <button onClick={handleToggle}>컴포넌트 토글</button>
-      {mounted && <Counter />}
-    </>
-  );
-};
+export default App;
--- 2-hook/src/pages/ProductPage/index.jsx
+++ 2-hook/src/pages/ProductPage/index.jsx
@@ -1,3 +1,40 @@
-const ProductPage = () => <>ProductPage</>;
+import React from "react";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import OrderableProductItem from "./OrderableProductItem";
+import ProductApi from "shared/api/ProductApi";
+
+const ProductPage = () => {
+  const [productList, setProductList] = React.useState([]);
+
+  const fetch = async () => {
+    try {
+      const productList = await ProductApi.fetchProductList();
+      setProductList(productList);
+    } catch (e) {
+      //   openDialog(<ErrorDialog />);
+      return;
+    }
+  };
+
+  React.useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <div className="ProductPage">
+      <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+        <ul>
+          {productList.map((product) => (
+            <li key={product.id}>
+              <OrderableProductItem product={product} />
+            </li>
+          ))}
+        </ul>
+      </Page>
+    </div>
+  );
+};
 
 export default ProductPage;
