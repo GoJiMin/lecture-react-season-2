dc04d7659fdb2ad8677aea7379a102a427ceca0c
GoJiMin <gojimin3095@gmail.com>
2024-11-12 22:01:33 +0900

    함수 컴포넌트로 바꾸기: OrderPage
    - 특이점 없이 이전과 동일하게 상태값은 useState, 부수효과는 useEffect을 사용해 바꿉니다.

--- 2-hook/src/pages/OrderPage/index.jsx
+++ 2-hook/src/pages/OrderPage/index.jsx
@@ -1,3 +1,42 @@
-const OrderPage = () => <>OrderPage</>;
+import React from "react";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import OrderStatusCard from "./OrderStatusCard";
+import OrderPaymentCard from "./OrderPaymentCard";
+import OrderDeliveryCard from "./OrderDeliveryCard";
+import OrderApi from "shared/api/OrderApi";
+
+const OrderPage = () => {
+  const [order, setOrder] = React.useState(null);
+
+  const fetch = async () => {
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      setOrder(order);
+    } catch (e) {
+      // openDialog(<ErrorDialog />)
+      return;
+    }
+  };
+
+  React.useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <div className="OrderPage">
+      <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
+        {order && (
+          <>
+            <OrderStatusCard order={order} />
+            <OrderPaymentCard order={order} />
+            <OrderDeliveryCard order={order} />
+          </>
+        )}
+      </Page>
+    </div>
+  );
+};
 
 export default OrderPage;
