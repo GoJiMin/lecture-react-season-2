ffd954302883eb67173b55e29ff461313add288d
GoJiMin <gojimin3095@gmail.com>
2024-11-15 17:42:12 +0900

    로그인 폼: 검증
    - 비제어 컴포넌트로 구현하지 않고 직접 상태를 가지는 제어 컴포넌트로 구현하면 저 상세한 검증 로직을 구현할 수 있음..
    - 보다 확장성 있고, 개발자가 원하는 검증 로직을 직접 구현이 가능함. 이 때 검증 로직 자체를 브라우저에 위임하지 않기 위해 form 태그에 noValidate 옵션을 줌..
    - 검증 로직은 그냥 내 맘대로 작성.. 이후 이 검증에 실패한다면 errors 상태에 추가해 UI에 즉각적으로 표시함..

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -14,11 +14,71 @@ import ProductPage from "./pages/ProductPage";
 
 // export default App;
 
+class Validation {
+  constructor(rules) {
+    this.rules = rules;
+  }
+
+  validateField(name, value) {
+    const fieldRules = this.rules[name];
+
+    if (!fieldRules) return "";
+
+    for (const rule of fieldRules) {
+      const errorMessage = rule(value);
+
+      if (errorMessage) return errorMessage;
+    }
+
+    return "";
+  }
+
+  validate(formState) {
+    const errors = {};
+
+    for (const name in formState) {
+      errors[name] = this.validateField(name, formState[name]);
+    }
+
+    return errors;
+  }
+}
+
+const rules = {
+  email: [
+    (value) => (!value ? "이메일을 입력해주세요." : ""),
+    (value) => {
+      const emailRegex = new RegExp(/^[^\s@]{6,}@[^\s@]+\.[^\s@]+$/);
+
+      if (!emailRegex.test(value)) {
+        return "유효하지 않은 이메일 형식입니다.";
+      } else {
+        return "";
+      }
+    },
+  ],
+  password: [
+    (value) => (!value ? "비밀번호를 입력해주세요." : ""),
+    (value) => (value.length < 6 ? "비밀번호는 6자 이상이어야 합니다." : ""),
+  ],
+};
+
 const LoginForm = () => {
   const [formState, setFormState] = React.useState({
     email: "",
     password: "",
   });
+
+  const [errors, setErrors] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const validator = new Validation(rules);
+
+  const validate = (formState) => {
+    return validator.validate(formState);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,11 +92,16 @@ const LoginForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const validationErrors = validate(formState);
+    setErrors(validationErrors);
+
+    if (Object.values(errors).some(Boolean)) return;
+
     console.log("Submitted", formState);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form noValidate onSubmit={handleSubmit}>
       <input
         type="email"
         name="email"
@@ -44,12 +109,14 @@ const LoginForm = () => {
         onChange={handleChange}
         autoFocus
       />
+      {errors.email && <p>{errors.email}</p>}
       <input
         type="password"
         name="password"
         value={formState.password}
         onChange={handleChange}
       />
+      {errors.password && <p>{errors.password}</p>}
       <button onClick={handleSubmit}>Login</button>
     </form>
   );
